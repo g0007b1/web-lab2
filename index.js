4d21bdad0fd67c1f44618a7928b7821b0126e7bc
g0007b1 <73489208+g0007b1@users.noreply.github.com>
2022-03-19 11:07:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ const contribution = document.getElementById("calculator__contribution");
 
 
 const refillableOptions = [
-    {months: 6, percent: 20, text: "6 месяцев - 20%"},
+    {months: 6, percent: 10, text: "6 месяцев - 20%"},
     {months: 12, percent: 22, text: "1 год - 22%"},
-    {months: 18, percent: 15, text: "1,5 года - 15%"},
-    {months: 24, percent: 10, text: "2 года - 10%"},
+    {months: 18, percent: 22.5, text: "1,5 года - 15%"},
+    {months: 24, percent: 20, text: "2 года - 10%"},
 ];
 
 const termOptions = [
-    {months: 3, percent: 20, text: "3 месяца - 20%"},
-    {months: 6, percent: 22, text: "6 месяцев - 22%"},
-    {months: 9, percent: 23, text: "9 месяцев - 23%"},
+    {months: 3, percent: 5, text: "3 месяца - 20%"},
+    {months: 6, percent: 11, text: "6 месяцев - 22%"},
+    {months: 9, percent: 17.25, text: "9 месяцев - 23%"},
     {months: 12, percent: 24, text: "1 год - 24%"},
-    {months: 18, percent: 18, text: "1,5 года - 18%"},
-    {months: 24, percent: 15, text: "2 года - 15%"},
+    {months: 18, percent: 27, text: "1,5 года - 18%"},
+    {months: 24, percent: 30, text: "2 года - 15%"},
 ];
 
 const onTypeChange = () => {
@@ -55,9 +55,13 @@ const calculateTerm = () => {
     let vklad;
     typeOfDeposit.value === "1" ? vklad = "Пополняемый" : vklad = "Срочный";
     if (contribution.value) {
-        let finalSolution = Number(terms.value / 100 * contribution.value) + Number(contribution.value);
-        calcSolution.textContent = `По вкладу "${vklad}" на срок и ставку ${terms.options[terms.selectedIndex].textContent}, при сумме ${contribution.value}`;
-        calcSolution2.textContent = `В конце срока вы получите ${finalSolution}р.`
+        if (contribution.value <= 0) {
+            calcSolution2.innerHTML = "<p style='color: red'> Введите правильную сумму!</p>"
+        } else {
+            let finalSolution = Number(terms.value / 100 * contribution.value) + Number(contribution.value);
+            calcSolution.textContent = `По вкладу "${vklad}" на срок и ставку ${terms.options[terms.selectedIndex].textContent}, при сумме ${contribution.value}`;
+            calcSolution2.textContent = `В конце срока вы получите ${finalSolution}р.`
+        }
     } else {
         calcSolution.innerHTML = "<p style='color: red'>Введите сумму вклада!</p>"
         calcSolution2.textContent = "";
